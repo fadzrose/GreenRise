6213b07c7d6bf0cabea523800acb0797369a2201
fadzrose <fadzillahroslan5903@gmail.com>
2025-01-06 10:10:19 +0800

    try 3.0

---
+++ floodScene/PlatformFlood.gd
@@ -0,0 +1,8 @@
+extends StaticBody2D
+
+func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
+	return true
+	return false
+
+func _drop_data(at_position: Vector2, data: Variant) -> void:
+	pass
